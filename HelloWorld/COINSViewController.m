37884babce1662750f0f6c5f37cbebed60bde2f0
山田悠加 <yamadaharuka@yamadayuuka-no-MacBook-Air.local>
2013-10-24 12:35:32 +0900

    test

--- HelloWorld/COINSViewController.m
+++ HelloWorld/COINSViewController.m
@@ -12,13 +12,53 @@
 
 @end
 
-@implementation COINSViewController
+@implementation COINSViewController {
+    int i;
+    UILabel *label;
+    UIImageView *imageView;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button setTitle:@"Hello World" forState:UIControlStateNormal];
+    button.frame = CGRectMake(10, 200, 300, 44);
+    [button addTarget:self action:@selector(changeText) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    label = [[UILabel alloc]initWithFrame:CGRectMake(0, 500, 600, 100)];
+    label.text = @"Sample";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor =[ UIColor redColor];
+    [self.view addSubview:label];
+
+    i = 0;
+    label.text = [NSString stringWithFormat:@"%d", i];
+    
+    imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"IMG_1220.jpg"  ]];
+    imageView.frame = CGRectMake(10, 10, 300, 200);
+    [self.view addSubview:imageView];
+    imageView.hidden = YES;
+    
+    
+    
+    
 }
+
+- (void)changeText
+{
+    i++;
+    label.text = [NSString stringWithFormat:@"%d", i];
+    if(i == 10) {
+        imageView.hidden = NO;
+    } else {
+        imageView.hidden = YES;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {
